d0d8346f958efe15809b902374eaf581356a69a3
AlyssiaPH <alyssia0207@gmail.com>
2023-11-21 15:30:57 +0100

    début formulaire

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+        <form className="App">
+            <h1>Banque du Nord de Snezhnaya</h1>
+            <p>
+                <div class={"inputDiv"}>
+                    <label for={"installment"}>Somme à payer / mois (Installment)</label>
+                    <input id={"installment"} type={"text"}/>
+                </div>
+                <div className={"inputDiv"}>
+                    <label for={"annual_log"}>Logarithme annuel (log.annual.in)</label>
+                    <input id={"annual_log"} type={"text"}/>
+                </div>
+                <div className={"inputDiv"}>
+                    <label for={"dti"}>Ration dette/revenu (Dti)</label>
+                    <input id={"dti"} type={"text"}/>
+                </div>
+                <div className={"inputDiv"}>
+
+                    <label for={"fico"}>Score Fico (300 à 850)</label>
+                    <input id={"fico"} type={"text"}/>
+                </div>
+                <div className={"inputDiv"}>
+
+                    <label for={"revol_bal"}>Solde renouvelable (revol_bal)</label>
+                    <input id={"revol_bal"} type={"text"}/>
+                </div>
+                <div className={"inputDiv"}>
+
+                    <label for={"revol_util"}>Taux d'utilisation du crédit (revol_util)</label>
+                    <input id={"revol_util"} type={"text"}/>
+                </div>
+                <div className={"inputDiv"}>
+
+                    <label for={"inq_last_6mth"}> Nombre de renseignements/plaintes (inq.last.6mths)</label>
+                    <input id={"inq_last_6mth"} type={"text"}/>
+                </div>
+                <div className={"inputDiv"}>
+
+                    <label for={"delinq_2yrs"}>Nombre de retard (delinq.2yrs)</label>
+                    <input id={"delinq_2yrs"} type={"text"}/>
+                </div>
+                <div className={"inputDiv"}>
+
+                    <label for={"pub_rec"}>Mombre de dossiers publics dérogatoires (pub.rec)</label>
+                    <input id={"pub_rec"} type={"text"}/>
+                </div>
+                <button>Simuler</button>
+            </p>
+
+        </form>
+        </>
+    );
 }
 
 export default App;
